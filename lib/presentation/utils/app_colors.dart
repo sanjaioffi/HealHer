21031afa4954143f4bc83b71c060fd61bc042161
Sanjai P <sanjaiprabhakaran.official@gmail.com>
2023-08-02 13:55:06 +0530

    added color constants

---
+++ lib/presentation/utils/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+  static const Color purplyBlue = Color(0xFF8a81f8);
+  static const Color heavyPurplyBlue = Color(0xFF7065e2);
+  static const Color babyPurplyBlue = Color(0xFF6af8dfe);
+  static const Color bluishCyan = Color(0xFF7ed1df);
+  static const Color bluishCyanLight = Color(0xFF7e2fbfc); 
+  static const Color blue = Color(0xFF72189fe); 
+  static const Color blueLight = Color(0xFF7d2e6fe); 
+  static const Color santaGrey = Color(0xFFa0a3b9);
+  static const Color lightPurplrBlue = Color.fromARGB(255, 229, 223, 254);
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+}
