4d9c28ba4fed0de4a1a456d866de3269ce95dfa9
sundhar22 <s.mohanasundharam2210@gmail.com>
2023-08-04 14:04:47 +0530

    common,journeys & themes  added

---
+++ lib/presentation/themes/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+  static const Color purplyBlue = Color(0xFF6f65dc);
+  static const Color bluishCyan = Color(0xFF7ed1df);
+  static const Color santaGrey = Color(0xFFa0a3b9);
+  static const Color lightPurpleBlue = Color.fromARGB(255, 229, 223, 254);
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+}
