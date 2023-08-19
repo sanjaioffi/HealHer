d9cf14c955a60b1bdfe0cbfa0a151aef46bbea37
Mitun Suresh <mitun.focused@gmail.com>
2023-08-19 10:01:40 +0530

    ui update

---
+++ lib/presentation/screens/onboarding/model/user_model.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  String userName = "Mitun Suresh";
+  DateTime userDOB = DateTime(2003, 05, 20);
+  bool isMale = false;
+  int userPeriodLength = 20;
+  DateTime lastPeriodDate = DateTime.now();
+  double height = 120;
+  double weight = 50;
+  bool isVegan = true;
+  bool isSouthIndian = false;
+  List<String> userTargets = [];
+  int userFitnessLevel = 0;
+  List<String> userMediacalConditon = [];
+  int userProfileChoice = 0;
+}
