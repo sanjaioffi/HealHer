cc8cd05363abedda48508947dedb343326bf57ff
Sundhar22 <mohanasundharamofficial@gmail.com>
2024-03-12 09:02:35 +0530

    Remove unused import statement

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:heal_her/features/periods/presentation/periods.dart';
+
 import 'config/dependencies/injection_container.dart';
 import 'config/routes/routes.dart';
 import 'config/theme/app_theme.dart';
